de2f9a65a28f94ba95b2f0d7e367bfd42d07898a
jkelsy <jkelsy@gmail.com>
2017-01-19 16:36:09 -0500

    Merge origin/master
    Conflicts:
    	src/main/webapp/pages/proyecto/proyectoEditInclude.xhtml

---
+++ src/main/java/j2o/software/kraken/service/facade/TerceroFacade.java
@@ -0,0 +1,26 @@
+package j2o.software.kraken.service.facade;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import j2o.software.kraken.db.Tercero;
+import j2o.software.kraken.service.facade.AbstractFacade;
+
+@Stateless
+@Named("tercero")
+public class TerceroFacade extends AbstractFacade<Tercero> {
+
+    @PersistenceContext(unitName = "KrakenPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TerceroFacade() {
+        super(Tercero.class);
+    }
+
+}
